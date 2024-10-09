15c6160816e6d37e89a80a9d71e3f78b35b482cd
Donovan Wright <donovan@datafleet.com>
2024-10-09 02:19:54 -0500

    Updating Email

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
-export const EMAIL: string = "UPDATEME@EMAIL.COM"
+export const EMAIL: string = "me@thedon.cloud"
 export const GITHUB_URL: string = "https://github.com/Donny208/"
 export const LINKEDIN_URL: string = "https://www.linkedin.com/in/dww/"
